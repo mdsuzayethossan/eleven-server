c0077e8c5d76e30d46c0d128ef86b2cf8535f52a
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-11-08 17:59:17 +0600

    add service

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q1ga5lh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+app.get("/", (req, res) => {
+  res.send("Home route");
+});
+async function run() {
+  const serviceCollection = client.db("dentalcare").collection("services");
+  app.post("/add-service", async (req, res) => {
+    const service = req.body;
+    const result = await serviceCollection.insertOne(service);
+    res.send(result);
+  });
+}
+run().catch((err) => console.error(err));
+app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  });
+});
