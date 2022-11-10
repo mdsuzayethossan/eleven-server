1fa5f2f91885e7a5147364c05fd18ce9f232001b
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-11-10 06:48:53 +0600

    gitignore customize

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ async function run() {
   const serviceCollection = client.db("dentalcare").collection("services");
   const reviewCollection = client.db("dentalcare").collection("reviews");
   app.post("/add-review", async (req, res) => {
-    const service = req.body;
-    const result = await reviewCollection.insertOne(service);
+    const review = req.body;
+    review.created_at = new Date();
+    const result = await reviewCollection.insertOne(review);
     res.send(result);
   });
+  //get logged user all review
   app.get("/reviews", async (req, res) => {
     let query = {};
     if (req.query.email) {
@@ -35,6 +37,18 @@ async function run() {
       };
     }
     const cursor = reviewCollection.find(query);
+    const myReviews = await cursor.toArray();
+    res.send(myReviews);
+  });
+  //get all review according to specific service
+  app.get("/reviews/:id", async (req, res) => {
+    let query = {};
+    var mysort = { created_at: -1 };
+    const id = req.params.id;
+    query = {
+      serviceId: id,
+    };
+    const cursor = reviewCollection.find(query).sort(mysort);
     const myReviews = await cursor.toArray();
     res.send(myReviews);
   });
@@ -64,18 +78,28 @@ async function run() {
   });
   app.post("/add-service", async (req, res) => {
     const service = req.body;
+    service.created_at = new Date();
     const result = await serviceCollection.insertOne(service);
     res.send(result);
   });
+  // app.post("/add-service", async (req, res) => {
+  //   const service = req.body;
+  //   const result = await serviceCollection.insertOne(
+  //     Object.assign({}, service, { created_at: new Date() })
+  //   );
+  //   res.send(result);
+  // });
   app.get("/services", async (req, res) => {
     const query = {};
-    const cursor = serviceCollection.find(query);
+    var mysort = { created_at: -1 };
+    const cursor = serviceCollection.find(query).sort(mysort);
     const services = await cursor.toArray();
     res.send(services);
   });
   app.get("/service", async (req, res) => {
     const query = {};
-    const cursor = serviceCollection.find(query).limit(3);
+    var mysort = { created_at: -1 };
+    const cursor = serviceCollection.find(query).sort(mysort).limit(3);
     const service = await cursor.toArray();
     res.send(service);
   });
